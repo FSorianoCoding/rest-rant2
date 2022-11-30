14c86b42bebe76533ca4db327e4584ec34970676
Fabian Soriano <fabo.soriano@gmail.com>
2022-11-29 20:26:09 -0800

    Adding styles to rest-rant2

--- index.js
+++ index.js
@@ -1,29 +1,24 @@
-// Require the environment variables
+// Modules and Globals
 require('dotenv').config()
-
-// Require needed node modules
 const express = require('express')
 const app = express()
 
-// Defining the view engine
+// Express Settings
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
-// Import router for places
+// Controllers & Routes
 app.use('/places', require('./controllers/places'))
 
-// Declare the routes
 app.get('/', (req, res) => {
-    // res.send('Hello World!')
-    // You don't have to specify the 'views' folder. 
-    // It already knows to look for a 'views' folder when you call the render method!
     res.render('home')
 })
 
-app.get('*', (req, res) => {
-    // res.status(404).send('<h1>404 Page</h1>')
+app.get('*', (req, res) => {    
     res.render('error404')
 })
 
-// Listen to a port number defined by a local environment variable
+// Listen for Connections
 app.listen(process.env.PORT)
