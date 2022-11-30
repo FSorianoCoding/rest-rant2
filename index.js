613815bff2b5bc75f010f0426ceefd905db449fd
Fabian Soriano <fabo.soriano@gmail.com>
2022-11-29 18:49:41 -0800

    Adding 404 page view

--- index.js
+++ index.js
@@ -21,7 +21,8 @@ app.get('/', (req, res) => {
 })
 
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
+    // res.status(404).send('<h1>404 Page</h1>')
+    res.render('error404')
 })
 
 // Listen to a port number defined by a local environment variable
